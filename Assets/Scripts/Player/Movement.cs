328043acec3e7eff63faafcf22e9a16ec2508e9c
josh <wilgjt10@uwgb.edu>
2023-01-23 12:34:18 -0600

    Added weight factor to collected materials

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    private BoxCollider2D coll;
-    private SpriteRenderer sprite;
-    private Animator anim;
-
-    public float dirX;
-    public float dirY;
-
     [SerializeField] private ItemAtlas atlas;
     [SerializeField] private LayerMask ground;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float playerToCeilGap = 0.1f;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+    private ItemClass[] minerals;
+    private bool countedRecently;
+    private float weight;
+
+    public float dirX;
+    public float dirY;
 
     private enum MovementState { idle, walking, flying, falling, drillup, drilldown, drillside }
 
@@ -27,6 +30,9 @@ public class Movement : MonoBehaviour
         coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        minerals = atlas.CreateInstance(ItemClass.ItemType.mineral);
+        countedRecently = false;
+        weight = 0;
     }
 
     // Update is called once per frame
@@ -39,16 +45,28 @@ public class Movement : MonoBehaviour
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         var amount = atlas.currentUpgradeAmounts[(int)ItemAtlas.UpgradeTypes.jetpack] / 5f;
+        if (!countedRecently)
+        {
+            countedRecently = true;
+            weight = CountMineralWeight();
+            Debug.Log(weight);
+            Invoke(nameof(UpdateRecently), 1);
+        }
         if (dirY > 0f && (!IsGrounded() || !IsACeiling()))
         {
-            rb.velocity = new Vector2(dirX * amount, dirY * amount);
+            rb.velocity = new Vector2(dirX * (amount / (weight + 1f)), dirY * (amount / (weight + 1f)));
         }
         else if (dirX != 0f && (!IsGrounded()))
         {
-            rb.velocity = new Vector2(dirX * amount, MathF.Abs(dirX / (amount *  20)));
+            rb.velocity = new Vector2(dirX * (amount / (weight + 1f)), MathF.Abs(dirX / (amount *  20f)));
         }
 
         UpdateAnimationState();
+    }
+
+    private void UpdateRecently()
+    {
+        countedRecently = false;
     }
 
     private void UpdateAnimationState()
@@ -117,5 +135,22 @@ public class Movement : MonoBehaviour
     private bool IsAWall(Vector2 direction)
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, direction, 0.1f, ground);
+    }
+
+    private float CountMineralWeight()
+    {
+        float counter = 0;
+
+        foreach (var item in minerals)
+        {
+            var amount = item.amountCollected;
+            if (amount > 0)
+            {
+                var log = Mathf.Pow(item.itemWorth, 0.01f) - 1;
+                Debug.Log("log: " + log);
+                counter += amount * log;
+            }
+        }
+        return counter;
     }
 }
