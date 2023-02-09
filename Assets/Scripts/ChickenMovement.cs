ef7a617c5ad19747579f945b6becc972518f7688
JoshWilger <hap3guy@gmail.com>
2023-02-09 17:21:07 -0600

    Added attack animation to boss

--- Assets/Scripts/ChickenMovement.cs
+++ Assets/Scripts/ChickenMovement.cs
@@ -6,23 +6,37 @@ public class ChickenMovement : MonoBehaviour
 {
     [SerializeField] private Collider2D playerColl;
     [SerializeField] private float movementSpeed;
+    [SerializeField] private float attackInterval;
+    [SerializeField] private float peckDelay;
+    [SerializeField] private float laserDelay;
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private Animator anim;
     private bool doingAttack;
+    private bool invoked;
 
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
         doingAttack = false;
+        invoked = false;
     }
 
     private void FixedUpdate()
     {
         var playerX = playerColl.bounds.center.x;
         var playerY = playerColl.bounds.center.y;
+
+        if (!invoked)
+        {
+            invoked = true;
+            anim.SetTrigger("run");
+            Invoke(nameof(Attack), attackInterval);
+        }
 
         if (playerX > transform.position.x - 1 && playerX < transform.position.x + 1)
         {
@@ -38,5 +52,42 @@ public class ChickenMovement : MonoBehaviour
             sprite.flipX = false;
             rb.velocity = new Vector2(-movementSpeed, rb.velocity.y);
         }
+    }
+
+    private void Attack()
+    {
+        doingAttack = true;
+
+        switch (Random.Range(1, 3))
+        {
+            case 1:
+                anim.SetTrigger("peck");
+                Invoke(nameof(Peck), peckDelay);
+                break;
+
+            case 2:
+                anim.SetTrigger("laser");
+                Invoke(nameof(Laser), laserDelay);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void Peck()
+    {
+
+
+        invoked = false;
+        doingAttack = false;
+    }
+
+    private void Laser()
+    {
+
+
+        invoked = false;
+        doingAttack = false;
     }
 }
