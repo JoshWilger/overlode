74df67ad0037041ff491303d5b29a33115a0fe6e
JoshWilger <hap3guy@gmail.com>
2023-03-05 16:32:02 -0600

    Fixed bug in object scrolling spawning

--- Assets/Scripts/SpawnScrollingObjects.cs
+++ Assets/Scripts/SpawnScrollingObjects.cs
@@ -21,10 +21,11 @@ public class SpawnScrollingObjects : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        var rightHeight = Mathf.FloorToInt(transform.position.y + 15);
+        var rightHeight = Mathf.FloorToInt(transform.position.y + 10);
         if (rightHeight > highestY && objects.Count < 100)
         {
-            for (int i = 0; i < rightHeight - highestY; i++)
+            var diff = rightHeight - highestY;
+            for (int i = 0; i < diff; i++)
             {
                 for (int a = 0; a < spawnLayerAttempts; a++)
                 {
@@ -35,20 +36,21 @@ public class SpawnScrollingObjects : MonoBehaviour
                         var newObject = Instantiate(spawnMe);
                         newObject.transform.position = new Vector3(newObject.transform.position.x, highestY + i, newObject.transform.position.z);
                         objects.Add(newObject);
-
                     }
                 }
             }
-            highestY = Mathf.FloorToInt(transform.position.y + 15);
+            highestY += diff;
+            Debug.Log(highestY);
         }
         else if (rightHeight < spawnMe.transform.position.y && objects.Count > 0)
         {
+            Debug.Log($"Destroying {objects.Count} {spawnMe.name}");
             foreach (var item in objects)
             {
                 Destroy(item);
             }
             objects.Clear();
-            highestY = Mathf.FloorToInt(transform.position.y);
+            highestY = Mathf.FloorToInt(spawnMe.transform.position.y);
         }
     }
 }
